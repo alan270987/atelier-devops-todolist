ba6cce6cf102e69a8d5d9e9adc66157616392fc2
jasonbe2504-blip <jason.be2504@gmail.com>
2026-03-04 16:25:27 +0100

    Ajout package-lock.json et correction workflow

---
+++ todolist.test.js
@@ -0,0 +1,19 @@
+// Une fonction simple pour tester
+function ajouterTache(tache) {
+    if (!tache) return false;
+    if (tache.length < 3) return false;
+    return true;
+}
+
+// Les tests
+test('ajoute une tâche valide', () => {
+    expect(ajouterTache('Apprendre DevOps')).toBe(true);
+});
+
+test('refuse une tâche vide', () => {
+    expect(ajouterTache('')).toBe(false);
+});
+
+test('refuse une tâche trop courte (moins de 3 caractères)', () => {
+    expect(ajouterTache('OK')).toBe(false);
+});
